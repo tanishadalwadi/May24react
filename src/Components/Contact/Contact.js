dc09e49c6c9576add03dc769bc2004fab802080e
Tanisha Dalwadi <tanishapdalwadi@gmail.com>
2021-05-24 19:27:51 -0700

    contact fix

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,8 +11,8 @@ function Contact() {
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com </p>
-          <p className="lead">Phone : 732 874 0188 </p>
+          <p className="lead">Email : tanishapdalwadi1415@gmail.com </p>
+          <p className="lead">Phone : 512 734 7918 </p>
           <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp  </p>
         </Container>
       </Jumbotron>
