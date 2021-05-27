25ea8f56ec3e479de59ac494fe07979e5e4fa420
Tanisha Dalwadi <tanishapdalwadi@gmail.com>
2021-05-27 10:59:20 -0700

    navbar

--- src/Components/Homeworks/index.js
+++ src/Components/Homeworks/index.js
@@ -21,6 +21,7 @@ function Homeworks() {
           <th>Type</th>
           <th>Description</th>
           <th>Link</th>
+          <th>Github Link</th>
         </tr>
       </thead>
       <tbody>
@@ -28,6 +29,7 @@ function Homeworks() {
           <th scope="row">1</th>
           <td>Homework1</td>
           <td>Git Practice. Provide link of your repo here </td>
+          <td>N/A</td>
           <td><a href="https://github.com/tanishadalwadi/homework1.git"> Link</a> </td>
        
       </tr>
@@ -36,6 +38,7 @@ function Homeworks() {
           <td>Homework 2</td>
           <td>Javascript - Here is my codepen link with solutions</td>
           <td><a href="https://codepen.io/tanishadalwadi/pen/gOmWyLM?editors=1112"> Link</a> </td>
+          <td>N/A</td>
        
       </tr>
       <tr>
@@ -43,6 +46,7 @@ function Homeworks() {
           <td>Homework 2.1</td>
           <td>Storage</td>
           <td><a href="https://codepen.io/tanishadalwadi/pen/OJpjNzQ?editors=0012"> Link</a> </td>
+          <td>N/A</td>
        
       </tr>
      
@@ -50,6 +54,7 @@ function Homeworks() {
           <th scope="row">4</th>
           <td>Homework 2.2</td>
           <td>HTML</td>
+          <td>N/A</td>
           <td><a href="https://github.com/tanishadalwadi/homework2.2.git"> Link</a> </td>
        
       </tr>
@@ -58,6 +63,15 @@ function Homeworks() {
           <td>Homework 3.1</td>
           <td>CSS Selector</td>
           <td><a href="https://codepen.io/tanishadalwadi/pen/xxqLMyw"> Link</a> </td>
+          <td>N/A</td>
+       
+      </tr>
+      <tr>
+          <th scope="row">6</th>
+          <td>Homework 3.3</td>
+          <td>Navbar</td>
+          <td><a href="https://navigationbar-tanisha.netlify.app/"> Link</a> </td>
+          <td><a href="https://github.com/tanishadalwadi/Navbar.git"> Link</a> </td>
        
       </tr>
         
